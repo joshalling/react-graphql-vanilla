496a0625865efd921ff5d954d62103e55d94aec0
Josh Alling <joshralling@gmail.com>
2018-11-13 13:46:04 -0700

    clean up App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,54 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+
+const axiosGithubGraphql = axios.create({
+    baseURL: "https://api.github.com/graphql",
+    headers: {
+        Authorization: `bearer ${process.env.GITHUB_ACCESS_TOKEN}`
+    }
+});
+
+const TITLE = "React GraphQL Github Client";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    state = {
+        path: "freeCodeCamp/freeCodeCamp"
+    };
+
+    componentDidMount() {
+        //fetch
+    }
+
+    onChange = event => {
+        this.setState({ path: event.target.value });
+    };
+
+    onSubmit = event => {
+        //fetch data
+
+        event.preventDefault();
+    };
+
+    render() {
+        const { path } = this.state;
+        return (
+            <div>
+                <h1>{TITLE}</h1>
+
+                <form onSubmit={this.onSubmit}>
+                    <label htmlFor="url">Show open issues for https://github.com/</label>
+                    <input
+                        id="url"
+                        type="text"
+                        value={path}
+                        onChange={this.onChange}
+                        style={{ width: "300px" }}
+                    />
+                    <button type="submit">Search</button>
+                </form>
+            </div>
+        );
+    }
 }
 
 export default App;
